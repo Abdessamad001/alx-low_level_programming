3e3b6f0ccc6fe6405d1c4a5240cf708c5ba1f3fb
abdessamad <abdessamad.asbbane@gmail.com>
2023-03-16 15:54:01 -0700

    commit task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle"); return 0;
+#include <stdio.h>
+/**
+*main - first task with betty style
+*return: return to 0 mean (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+/**
+*return: return to the 0
+*/
+return (0);
+}
