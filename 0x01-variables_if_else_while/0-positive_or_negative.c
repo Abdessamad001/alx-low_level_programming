d0051c93e1d581687bc6c9025292987fa7552ca8
abdessamad <abdessamad.asbbane@gmail.com>
2023-03-17 10:39:08 -0700

    commit task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
-* main - task 0 code 
-*
-* Return: Always 0
+ * main - Entry point
+ * Description: this program can check for the positive and negative numbers
+ * Return: always 0
 */
 int main(void)
 {
 	int n;
 
-    while (1)
-    {
-	 printf("Please enter a number: ");
-	 scanf("%d", &n);
-
-        if (n == 0)
-        {
-            printf("%d is zero\n", n);
-        }
-        else if (n > 0)
-        {
-            printf("%d is positive\n", n);
-        }
-        else
-        {
-            printf("%d is negative\n", n);
-        }
-    }
-
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
 }
-
